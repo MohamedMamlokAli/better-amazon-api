293fa53e25842158ff20a959fda7f647d57f732b
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-10-01 20:04:20 +0200

    ADD register controller functionality

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,16 @@
 const asyncwrapper = require('../middlewares/async');
-
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 const register = asyncwrapper(async (req, res) => {
-  res.status(201).json({ msg: 'Account created.' });
+  const user = await User.create(req.body);
+  const token = jwt.sign(
+    { id: user._id, name: user.name, image: user.image },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '30d',
+    }
+  );
+  res.status(201).json({ msg: 'Account created.', user, token });
 });
 const login = asyncwrapper(async (req, res) => {
   res.status(201).json({ msg: 'Account Logged in.' });
