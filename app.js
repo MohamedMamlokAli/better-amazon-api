c0a48532f26bb4db687eb84e42c09b6e3949e1f6
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-10-01 21:39:45 +0200

    ADD error handling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const connect = require('./db/connect');
 const app = express();
 const productsRoutes = require('./routes/products');
 const userRouters = require('./routes/user');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 //middlewares
 app.use(express.json());
@@ -11,6 +12,7 @@ app.use(express.json());
 app.get('/', (req, res) => res.send('<h1>Better Amazon API</h1>'));
 app.use('/api/v1', productsRoutes);
 app.use('/api/v1/user', userRouters);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
