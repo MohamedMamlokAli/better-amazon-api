7bfc18c1bff91e5c1511b32ba059da6746716fe2
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-10-02 14:37:08 +0200

    ADD update products controller functionality

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,7 @@
 const asyncWrapper = require('../middlewares/async');
 const Product = require('../models/product');
 const filter = require('../utils/filter');
+const { UnauthenticatedError } = require('../errors');
 const getProducts = asyncWrapper(async (req, res) => {
   let num = await Product.count();
   let filters = filter(req.query);
@@ -24,22 +25,38 @@ const getProduct = asyncWrapper(async (req, res) => {
   res.status(200).json({ product });
 });
 const addProduct = asyncWrapper(async (req, res) => {
-  const product = await Product.create(req.body);
+  const productDetails = { ...req.body, productSeller: req.user.id };
+  const product = await Product.create(productDetails);
   res.status(201).json({ product });
 });
 const patchProduct = asyncWrapper(async (req, res) => {
+  const { id: seller } = req.user;
   const { id } = req.params;
-  const product = await Product.findByIdAndUpdate(id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  const product = await Product.findOneAndUpdate(
+    { _id: id, productSeller: seller },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  console.log(product);
+  if (!product) {
+    throw new UnauthenticatedError('Authorization failed.');
+  }
   res.status(200).json({ product });
 });
 const deleteProduct = asyncWrapper(async (req, res) => {
+  const { id: seller } = req.user;
   const { id } = req.params;
-  // await Product.deleteMany();
-  // res.status(200).json({ msg: 'deleted' });
-  const product = await Product.findByIdAndDelete(id);
+  const product = await Product.findOneAndDelete({
+    _id: id,
+    productSeller: seller,
+  });
+  console.log(product);
+  if (!product) {
+    throw new UnauthenticatedError('Authorization failed.');
+  }
   res.status(200).json({ product });
 });
 
